70f18ebfc2f60fc30458855d7f343dc6a7a52be1
siefmansour11 <siefmansoursys@gmail.com>
2024-11-03 16:31:10 +0200

    first

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int arr[]= {7,10,2,8,0};
+    int n =sizeof(arr)/sizeof(int);
+    for( int a= 0; a< n-1; a++)
+    {
+        for(int b=a+1; b,n; b++)
+        {
+            if(arr[a]>arr[b])
+            {
+                int temp =arr[a];
+                arr[a]=arr[b];
+                arr[b]=temp;
+            }
+        }
+    }
+    for(int i=0; i<n; i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
